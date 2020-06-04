1fd20525f2de47210b37a73769759ca3c5caee0d
alexhongo <38734290+alexhongo@users.noreply.github.com>
2020-06-05 00:25:45 +0300

    Update scriptss.js

--- scriptss.js
+++ scriptss.js
@@ -1,5 +1,7 @@
-let fullname = 'Alex Okwach Hongo';
+const fullname = 'Alex Okwach Hongo';
 
-let id = 'HNG-00787';
+const id = 'HNG-00787';
+
+const email = 'alexokwach@gmail.com';
 
 console.warn("Hello World, this is " + fullname + " with HNGi7 ID " + id + " using javascript for stage 2 task");
